c16aad62e79f5bc714e114db716f49be695b44c2
Aaron Janke <janke.aaron@gmail.com>
2018-10-30 14:57:51 -0400

    removed chai

--- test.js
+++ test.js
@@ -1,15 +1,19 @@
-let { expect } = require("chai");
+// let { expect } = require("chai");
+
+const assert = require("assert");
 
 let { maxProduct } = require("./problem1");
 let { isAnagram } = require("./problem2");
 
 it("maxProduct works", function() {
   let numbers = [3, -29, 44, -800, 333, -700];
-  expect(maxProduct(numbers)).equal(560000);
+  // expect(maxProduct(numbers)).equal(560000);
+  assert.strictEqual(maxProduct(numbers), 560000);
 });
 
 it("anagram detection working", function() {
   let str1 = "schoolmaster";
   let str2 = "theclassroom";
-  expect(isAnagram(str1, str2)).equal(true);
+  // expect(isAnagram(str1, str2)).equal(true);
+  assert.strictEqual(isAnagram(str1, str2), true);
 });
